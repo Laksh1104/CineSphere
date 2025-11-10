3650bc6190f0158593f8f0e4464c54ed7980c25d
Laksh1104 <laksh.boodnah@mail.utoronto.ca>
2025-11-10 14:24:46 -0500

    Added folders accorrding to  Clean Architecture and moved Landing Page into its appropriate view

--- src/main/java/entity/Movie.java
+++ src/main/java/entity/Movie.java
@@ -1,4 +1,12 @@
 package entity;
 
 public class Movie {
+    private final int film_id;
+    private final String film_name;
+
+    public Movie(int filmId, String filmName) {
+        film_id = filmId;
+        film_name = filmName;
+    }
+
 }
--- src/main/java/view/BookingView.java
+++ src/main/java/view/BookingView.java
@@ -1,4 +1,49 @@
 package view;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class BookingView {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Background Display");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900, 800);
+        JPanel backgroundPanel = new JPanel();
+        backgroundPanel.setBackground(new Color(255, 255, 224));
+        backgroundPanel.setLayout(new BoxLayout(backgroundPanel, BoxLayout.Y_AXIS));
+
+        JLabel title = new JLabel("CineSphere", SwingConstants.CENTER);
+        title.setForeground(Color.BLACK);
+        title.setFont(new Font("Arial", Font.BOLD, 20));
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+
+        buttonPanel.setBackground(new Color(255, 255, 224));
+        buttonPanel.setPreferredSize(new Dimension(800, 50));
+        buttonPanel.setMaximumSize(new Dimension(800, 50));
+        buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JButton watchlistButton = new JButton("Watchlist");
+        JButton bookButton = new JButton("Book");
+        JButton logoutButton = new JButton("Logout");
+        JButton homeButton = new JButton("Home");
+        buttonPanel.add(title);
+        buttonPanel.add(homeButton);
+        buttonPanel.add(watchlistButton);
+        buttonPanel.add(bookButton);
+        buttonPanel.add(logoutButton);
+
+        JPanel headerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        headerPanel.setBackground(new Color(255, 255, 224));
+        JLabel titleLabel = new JLabel("Booking Page");
+        titleLabel.setFont(new Font("Calibri", Font.BOLD, 20));
+        headerPanel.add(titleLabel);
+
+        JPanel headerButtonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        headerButtonPanel.setBackground(new Color(255, 255, 224));
+
+        String[] movies = {"Movies", "YAYAYA", "SNNAN"};
+        JComboBox<String> movieDropdown = new JComboBox<>(movies);
+        headerButtonPanel.add(movieDropdown);
+
+    }
 }
